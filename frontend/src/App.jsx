010139fd0dbecd6a108922bd683b5dd781c4df80
FellipeCastro <fellipecastro27d@gmail.com>
2024-03-04 17:52:45 -0300

    Base finalizada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,27 +6,29 @@ function App() {
     <>
       <div className="form-container">
         <form method="post" autoComplete='off'>
-          <fieldset>
-            <legend>Cadastre-se</legend>
+          <h1>Cadastre-se</h1>
 
-            <div className="input-container">
-              <label htmlFor="nome">Nome: </label>
-              <input type="text" name="nome" id="nome" placeholder='Digite seu nome aqui' required />
-            </div>
-            <div className="input-container">
-              <label htmlFor="email">E-mail: </label>
-              <input type="email" name="email" id="email" placeholder='Digite seu e-mail aqui' required />
-            </div>
-            <div className="input-container">
-              <label htmlFor="password">Senha: </label>
-              <input type="password" name="password" id="password" placeholder='Digite sua senha aqui' required />
-            </div>
-            <div className="input-container">
-              <label htmlFor="confirm-password">Confirme sua senha: </label>
-              <input type="password" name="consfirm-password" id="confirm-password" placeholder='Confirme sua senha aqui' required />
-            </div>
-            <button type="submit" className='btn-submit'>Cadastrar</button>
-          </fieldset>
+          <div className="input-container">
+            <label htmlFor="nome">Nome: </label>
+            <input type="text" name="nome" id="nome" placeholder='Digite seu nome aqui' required />
+            <span></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="email">E-mail: </label>
+            <input type="email" name="email" id="email" placeholder='Digite seu e-mail aqui' required />
+            <span></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="password">Senha: </label>
+            <input type="password" name="password" id="password" placeholder='Digite sua senha aqui' required />
+            <sapn></sapn>
+          </div>
+          <div className="input-container">
+            <label htmlFor="confirm-password">Confirme sua senha: </label>
+            <input type="password" name="consfirm-password" id="confirm-password" placeholder='Confirme sua senha aqui' required />
+            <span></span>
+          </div>
+          <button type="submit" className='btn-submit'>Cadastrar</button>
         </form>
       </div>
     </>
